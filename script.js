d37c290ea329524e44452b86cfa448acfcad2aa4
vinpstrn <vinpstrn@gmail.com>
2025-02-26 10:54:32 -0600

    Added links with smooth scroll

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Nav smooth scrolling 
+document.querySelector('.nav__list').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const el = e.target.closest('.nav__link');
+
+  if(!el) return;
+
+  const id = el.getAttribute('href');
+  document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+});
+
+
+// Projects tabs
 const tab = document.querySelector('.projects__tab');
 const tabBtn = document.querySelectorAll('.projects__tab-btn');
 const tabContent = document.querySelectorAll('.projects__content');
@@ -21,4 +35,5 @@ tab.addEventListener('click', (e) => {
   document.querySelector(`.projects__content--${clicked.dataset.btn}`).classList.add('projects__content--active');
 
   console.log(clicked);
-})
+});
+
